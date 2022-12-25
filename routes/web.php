1df1675582eaa3cf01aa68ea2671d5cbd11f37b4
hilmI@gementar.com <hilmI@gementar.com>
2022-12-25 15:30:38 +0800

    added template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        $user = Auth::user();
+        if ($user->category == 'admin') {
+            return redirect()->route('dashboard');
+        } elseif ($user->category == 'lecturer') {
+            return redirect()->route('welcome');
+        }
+    }
+    return view('sign-in');
 });
